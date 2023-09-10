ebda3a5d0755cf9dacd71e055513de7c11ba3d38
Artem Serebriakov <koba156@yandex.ru>
2023-09-10 21:51:19 +0300

    Готовое задание №1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,26 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+    let arr = [];
+    let d = b ** 2 - 4 * a * c;
+    let rootOfEquation, secondRootOfEquation;
+    if (d > 0) {
+        rootOfEquation = (-b + Math.sqrt(d)) / (2 * a);
+        secondRootOfEquation = (-b - Math.sqrt(d)) / (2 * a);
+        arr.push(rootOfEquation, secondRootOfEquation);
+    } else if (d === 0) {
+        rootOfEquation = -b / (2 * a);
+        arr.push(rootOfEquation);
+    }
+    return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+    percent = (percent / 100) / 12;
+    let S = amount - contribution;
+    let payment = S * (percent + (percent / (((1 + percent) ** countMonths) - 1)));
+    let totalAmount = payment * countMonths;
+    totalAmount = totalAmount.toFixed(2);
+    totalAmount = +totalAmount;
+    return totalAmount;
 }
